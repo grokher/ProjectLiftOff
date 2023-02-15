f08ee524f1db1dcca21a20aa4f6c833ae14d1f60
grokher <47322787+grokher@users.noreply.github.com>
2023-02-15 15:58:54 +0100

    left and right movement enemy
    up and down still in the works

--- GXPEngine2023/GXPEngine/Enemy.cs
+++ GXPEngine2023/GXPEngine/Enemy.cs
@@ -2,66 +2,59 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
-namespace GXPEngine
-{
-    
+using GXPEngine;
 
     class Enemy : Sprite
     {
         bool gameIsPlaying = false;
         bool IsmovingX = true;
         int enemyLocationX;
+        int enemyLocationY = 5;
+
+        Random RNG = new Random();
 
         public Enemy() : base("triangle.png")
         {
             SetScaleXY(0.5f, 0.5f);
-            SetXY(1920 / 2, 1080 / 2);
-            
+            SetXY(RNG.Next(0,1920), height / 2);
+
             //EnemyMovement();
         }
 
         void Update()
         {
             EnemyMovement();
-            Console.WriteLine(enemyLocationX);
         }
     
         public void EnemyMovement()
         {
-            
+
             if (!gameIsPlaying)
             {
-                if(enemyLocationX <= 1200 && IsmovingX)
+                Console.WriteLine(enemyLocationX);
+                if(x <= 1920 && IsmovingX)
                 {
-                    x = enemyLocationX += 1 * Time.deltaTime / 2;
-                    if(enemyLocationX > 1080)
-                    {
-                        IsmovingX = false;
-                        Console.WriteLine(IsmovingX);
-                        if (!IsmovingX)
-                        {
-                            x = enemyLocationX += -2 * Time.deltaTime / 2;
-                        }
-                    }
-                }
-
-                /*if(enemyLocationX >= 1080 && !IsmovingX)
-                {
-                    Console.WriteLine("moving Left");
-                    x = enemyLocationX -= 1 * Time.deltaTime;
-                    if(enemyLocationX < 0)
+                    //enemyLocationX = RNG.Next(0,1920);
+                    x = enemyLocationX += 1 * Time.deltaTime;
+                    if(enemyLocationX >= 1920)
                     {
                         IsmovingX = false;
                     }
-                }*/
-
+                    y = enemyLocationY / 4 + RNG.Next(1, 1080);
             }
-            //x++ * Time.deltaTime;
+                else if(x > 0 && !IsmovingX)
+                {
+                    x = enemyLocationX -= 1 * Time.deltaTime;
+                    if(enemyLocationX <= 0)
+                    {
+                        IsmovingX = true;
+                    }
+                    y = enemyLocationY / 4 + RNG.Next(1, 1080);
+            }
+            }
         }
         //enemy for now is triangle.png
         //insert spawning
 
         //insert firing at Player
     }
-}
